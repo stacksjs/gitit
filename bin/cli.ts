359b643959940c0bca62dd127ad1f20f186468c9
Chris <chrisbreuer93@gmail.com>
2025-04-22 13:01:56 -0700

    chore: wip

--- bin/cli.ts
+++ bin/cli.ts
@@ -7,19 +7,24 @@ import { version } from '../package.json'
 const cli = new CAC('gitit')
 
 cli
-  .command('template [template] [dir]', 'Clone a template from a repository')
+  .command('[template] [dir]', 'Clone a template from a repository')
+  .alias('clone [template] [dir]')
+  .alias('create [template] [dir]')
+  .alias('new [template] [dir]')
+  .option('--template <template>', 'Template to clone')
+  .option('--dir <dir>', 'Directory to clone the template into')
   .option('--force', 'Clone to existing directory even if exists')
   .option('--force-clean', 'Remove any existing directory or file recursively before cloning')
   .option('--shell', 'Open a new shell with current working directory')
   .option('--install', 'Install dependencies after cloning')
-  .option('--verbose', 'Show verbose debugging info')
   .option('--command <command>', 'Custom command to run after template is cloned')
   .option('--auth <token>', 'Custom Authorization token to use for downloading template')
   .option('--cwd <dir>', 'Set current working directory to resolve dirs relative to it')
   .option('--offline', 'Do not attempt to download and use cached version')
   .option('--prefer-offline', 'Use cache if exists otherwise try to download')
-  .example('gitit template github:user/repo my-project')
-  .example('gitit template github:user/repo my-project --command "npm run dev"')
+  .option('--verbose', 'Show verbose debugging info')
+  .example('gitit github:user/repo my-project')
+  .example('gitit github:user/repo my-project --command "npm run dev"')
   .action(async (template?: string, dir?: string, options?: GitItOptions) => {
     if (!template) {
       console.error('Missing template argument')
